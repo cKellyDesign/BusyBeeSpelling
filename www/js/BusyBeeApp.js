ec8d282c89ace2274a6f2833eed8c0d294c5f8a9
Conor Kelly <ckelly@ckellydesign.net>
2015-06-04 20:34:39 -0700

    Init Controller Hookup to Markup

--- www/js/BusyBeeApp.js
+++ www/js/BusyBeeApp.js
@@ -24,13 +24,45 @@ BusyBeeSpelling.controller('levelSelectControl', function($scope, $rootScope){
 
 BusyBeeSpelling.controller('levelControl', function($scope, $rootScope){
   $scope.message = "Welcome to level 1!";
-  $scope.levelLetters = [];
+
+  // For Generating letters for the level
+  $scope.letterLegend = {
+    "lowercase" : "abcdefghijklmnopqrstuvwxyzaeiouaeiouaeiouaeiou",
+    "UPPERCASE" : "ABCDEFGHIJKLMNOPQRSTUVWXYZAEIOUAEIOUAEIOUAEIOU",
+    "vowels" : "abcdefghijklmnopqrstuvwxyzaeiouaeiouaeiouaeiou",
+  };
+  $scope.levelLetters = []; // For binding letters
+
+  // CSS class names for different flowers
+  $scope.flowerLegend = ['flower-reddaisy', 'flower-purpletulip', 'flower-yellowdahlia'];
+
+  // Number values for Left / Top CSS properties
+  $scope.beePosition = {
+    "Left": 10,
+    "Top": 10
+  };
 
   $rootScope.$watch('currentLevel',
     function(currentLevel){
       $scope.generateLetters(currentLevel);
     }
   );
+
+  $scope.moveBee = function(e) {
+    // Handles screen tap event
+    e.stopPropagation();
+
+  };
+
+  $scope.collectLetter = function(e) {
+    // Handles letter tap event
+    e.stopPropagation();
+
+  };
+
+  $scope.checkAnswer = function() {
+    // Checks answer of collected letter
+  }
 
   $scope.backToMenu = function() {
     $rootScope.state.levelControl = false;
@@ -40,14 +72,8 @@ BusyBeeSpelling.controller('levelControl', function($scope, $rootScope){
 
   $scope.generateLetters = function(level) {
     if (!level) return;
-    var key = {
-      "lowercase" : "abcdefghijklmnopqrstuvwxyzaeiouaeiouaeiouaeiou",
-      "UPPERCASE" : "ABCDEFGHIJKLMNOPQRSTUVWXYZAEIOUAEIOUAEIOUAEIOU",
-      "vowels" : "abcdefghijklmnopqrstuvwxyzaeiouaeiouaeiouaeiou"
-    };
 
-    var possible = key[level];
-    $scope.levelLetters = [];
+    var possible = $scope.letterLegend[level];
 
     for ( var i=0; i < 6; i++ ) {
       $scope.levelLetters.push(possible.charAt(Math.floor(Math.random() * possible.length)));
